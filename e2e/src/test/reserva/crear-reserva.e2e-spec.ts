66f919069a3dd8efc51b97c7a0c05940279f12be
Sebastían Girón Arcila - Ceiba Software <sebastian.giron@ceiba.com.co>
2021-11-26 08:22:42 -0500

    configuracion jenkins y sonar

--- e2e/src/test/reserva/crear-reserva.e2e-spec.ts
+++ e2e/src/test/reserva/crear-reserva.e2e-spec.ts
@@ -2,6 +2,7 @@
 import { CrearReserva } from '../../page/reserva/crear-reserva.po';
 import { AppPage } from '../../app.po';
 import { ToolBarPage } from '../../page/toolbar/toolbar.po';
+import { browser } from 'protractor';
 
 
 
@@ -20,41 +21,67 @@ describe('Crear reserva',  () => {
     
     });
 
-    it('Deberia crear la reserva', async () => {
+    it('Deberia crear la reserva', () => {
         const NOMBRE = 'Mauricio';
         const CEDULA = '1118589632';
         const CANT_PERSONAS=2;
         const TELEFONO  = '3053198749';
 
-        await page.navigateTo();
-        await menu.clickBotonReserva();
-        await menu.clickBotonCrearReserva();
-        await crearReserva.clickInputNombre();
-        await crearReserva.ingresarNombre(NOMBRE);
-        await crearReserva.clickInputCedula();
-        await crearReserva.ingresarCedula(CEDULA);
-        await crearReserva.clickInputGlamping();
-        await crearReserva.clickOpcionIdGlamping();
-        await crearReserva.clickPickerFechaEntrada();
-        await crearReserva.clickAniosPickerFechaEntrada();
-        await crearReserva.clickBotonAdelanteAniosPickerFechaEntrada();
-        await crearReserva.clickBotonAnioPickerFechaEntrada();
-        await crearReserva.clickBotonMesPickerFechaEntrada();
-        await crearReserva.clickBotonDiaPickerFechaEntrada();
-        await crearReserva.clickPickerFechaSalida();
-        await crearReserva.clickAniosPickerFechaSalida();
-        await crearReserva.clickBotonAdelanteAniosPickerFechaSalida();
-        await crearReserva.clickBotonAnioPickerFechaSalida();
-        await crearReserva.clickBotonMesPickerFechaSalida();
-        await crearReserva.clickBotonDiaPickerFechaSalida();        
-        await crearReserva.clickInputCantPersonas();
-        await crearReserva.ingresarCantPersonas(CANT_PERSONAS)
-        await crearReserva.clickInputTelefono();
-        await crearReserva.ingresarTelefono(TELEFONO);
-
-        await crearReserva.clickBotonGuardarReserva();
-        const alerta = await crearReserva.getTextoSwal();
+        page.navigateTo();
+        browser.sleep(1000)
+         menu.clickBotonReserva();
+        browser.sleep(1000)
+         menu.clickBotonCrearReserva();
+        browser.sleep(1000)
+         crearReserva.clickInputNombre();
+        browser.sleep(1000)
+         crearReserva.ingresarNombre(NOMBRE);
+        browser.sleep(1000)
+         crearReserva.clickInputCedula();
+        browser.sleep(1000)
+         crearReserva.ingresarCedula(CEDULA);
+        browser.sleep(1000)
+         crearReserva.clickInputGlamping();
+        browser.sleep(1000)
+         crearReserva.clickOpcionIdGlamping();
+        browser.sleep(1000)
+         crearReserva.clickPickerFechaEntrada();
+        browser.sleep(1000)
+         crearReserva.clickAniosPickerFechaEntrada();
+        browser.sleep(1000)
+         crearReserva.clickBotonAdelanteAniosPickerFechaEntrada();
+        browser.sleep(1000)
+         crearReserva.clickBotonAnioPickerFechaEntrada();
+        browser.sleep(1000)
+         crearReserva.clickBotonMesPickerFechaEntrada();
+        browser.sleep(1000)
+         crearReserva.clickBotonDiaPickerFechaEntrada();
+        browser.sleep(1000)
+         crearReserva.clickPickerFechaSalida();
+        browser.sleep(1000)
+         crearReserva.clickAniosPickerFechaSalida();
+        browser.sleep(1000)
+         crearReserva.clickBotonAdelanteAniosPickerFechaSalida();
+        browser.sleep(1000)
+         crearReserva.clickBotonAnioPickerFechaSalida();
+        browser.sleep(1000)
+         crearReserva.clickBotonMesPickerFechaSalida();
+        browser.sleep(1000)
+         crearReserva.clickBotonDiaPickerFechaSalida();   
+        browser.sleep(2000)     
+         crearReserva.clickInputCantPersonas();
+        browser.sleep(1000)
+         crearReserva.ingresarCantPersonas(CANT_PERSONAS)
+        browser.sleep(1000)
+         crearReserva.clickInputTelefono();
+        browser.sleep(1000)
+         crearReserva.ingresarTelefono(TELEFONO);
+        browser.sleep(1000)
+         crearReserva.clickBotonGuardarReserva();
+        browser.sleep(1000)
+        const alerta =  crearReserva.getTextoSwal();
         expect(alerta).toEqual(RESERVA_CREADA);
+        browser.sleep(1000)
     
         
     })
